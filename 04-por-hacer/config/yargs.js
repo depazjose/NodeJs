90d59cae7cea1187bd5a20f86414f86c878a9b1e
depazjose <jose.depaz@gmail.com>
2019-05-01 01:43:14 -0500

    tarea 4

---
+++ 04-por-hacer/config/yargs.js
@@ -0,0 +1,27 @@
+const argv = require('yargs')
+    .command('actualizar', 'Actualiza el estado completado de la tarea', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripción de la tarea por hacer'
+        },
+        completado: {
+            default: true,
+            alias: 'c',
+            desc: 'Marca como completado o pendiente la tarea'
+        }
+    })
+    .command('crear', 'Crea un elmento por hacer', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripción de la tarea por hacer'
+        }
+    })
+    .help()
+    .argv;
+
+
+module.exports = {
+    argv
+}
