741f77b2eadbe9abfad107b9df59f6dfc9c907f5
depazjose <jose.depaz@gmail.com>
2019-05-01 20:14:12 -0500

    Sección 2, clase 45

--- 04-por-hacer/app.js
+++ 04-por-hacer/app.js
@@ -1,15 +1,24 @@
-const argv = require('yargs').argv;
-const config = require('./config/yargs.js');
+//const argv = require('yargs').argv;
+const argv = require('./config/yargs.js').argv;
+const porHacer = require('./por-hacer/por-hacer.js');
+const colors = require('colors');
 
 
 let comando = argv._[0];
 
 switch (comando) {
     case 'crear':
-        console.log('Crear por hacer');
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
         break;
     case 'listar':
-        console.log('Mostrar todas las tareas por hacer');
+        let listado = porHacer.getListado();
+        for (let tarea of listado) {
+            console.log('============Por hacer============='.green);
+            console.log(tarea.descripcion);
+            console.log('Estado: ', tarea.completado);
+            console.log('=================================='.green);
+        }
         break;
     case 'actualizar':
         console.log('Actualiza una tarea por hacer');
---
+++ 04-por-hacer/por-hacer/por-hacer.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDb = () => {
+    let data = JSON.stringify(listadoPorHacer);
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error('No se logró guardar', err);
+    });
+}
+
+const cargarDb = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+}
+
+const getListado = () => {
+    cargarDb();
+    return listadoPorHacer;
+}
+
+const crear = (descripcion) => {
+
+    cargarDb();
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+    guardarDb();
+    return porHacer;
+}
+
+module.exports = {
+    crear,
+    getListado
+}
