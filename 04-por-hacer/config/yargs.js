8b84de7127e0171715f702fc5d5565c016b8feca
depazjose <jose.depaz@gmail.com>
2019-05-01 20:48:10 -0500

    Sección 2, clase 48

--- 04-por-hacer/config/yargs.js
+++ 04-por-hacer/config/yargs.js
@@ -1,29 +1,25 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+};
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea'
+};
+
 const argv = require('yargs')
     .command('actualizar', 'Actualiza el estado completado de la tarea', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea por hacer'
-        },
-        completado: {
-            default: true,
-            alias: 'c',
-            desc: 'Marca como completado o pendiente la tarea'
-        }
+        descripcion,
+        completado
     })
     .command('crear', 'Crea un elmento por hacer', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea por hacer'
-        }
+        descripcion
     })
     .command('borrar', 'Borra un elmento por hacer', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea por borrar'
-        }
+        descripcion
     })
     .help()
     .argv;
